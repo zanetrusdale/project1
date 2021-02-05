10dec4b47861fccaaf9fb9e6ca43e4b137f1292e
Zane <zanetrusdale03@gmail.com>
2021-02-05 16:50:24 +0300

    Changed: Product expire dates {from ArrayList to Array} - All thanks to Teo & Preslava
    Added:
    Arrived dates

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,13 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         while (true){
-            System.out.println("Избери една опция:");
+            System.out.println("Днешна дата: 13.02\n");             // With this date i check my code for errors with expire dates, expire dates ect...
+                System.out.println("Избери една опция:");
             System.out.println("---------------------------\n"+
-                    "   1. Продукти, които предлагаме \n" + //Products that we provide and their descriptions:
-                    "   2. Налични продукти \n" +          //Products that we have at the moment:
-                    "   3. Изход \n" +
+                    "   1. Продукти, които предлагаме \n" +       //Products that we provide and their descriptions: - (you can add order based on  this products)
+                    "   2. Налични продукти \n" +                //Products that we have at the moment:
+                    "   3. Разпределение на склада \n" +        //Where the product is placed
+                    "   4. Изход \n" +
                     "----------------------------" );
             byte userChoise = input.nextByte();
             switch (userChoise) {
@@ -21,39 +23,37 @@ public class Main {
                 case 2:
                     showAvailableProducts();
                 case 3:
+                    showThePlacementOfTheProducts(); // Place of the products:
+                case 4:
                     exitProgram();
                     break;
                 default:
                     do {
                         System.out.println("Грешен избор, моля изберете пак:");
                          userChoise = input.nextByte();
-                    } while (userChoise > 3 || userChoise < 1);
+                    } while (userChoise > 4 || userChoise < 1);
             }
         }
 
     }
 
-    public static void showAvailableProducts(){
-        ArrayList<String> productsAvailable = new ArrayList<String>();
-        productsAvailable.add("\n, - Кока Кола \n");
-        productsAvailable.add("- Закуски \n");
-        productsAvailable.add("- Водка \n");
+    public static void showAvailableProducts(){                             //Method for User's choise "2"
+
+        String availableProducts [] = {"Кока Кола","Закуски","Водка"};
+
+        Double dateProductArrived [] = {5.02, 13.02, 1.02};
+
+        String expireDates [] = {"20 days", "3 days", "50 days"};
 
 
-        ArrayList<String> expireDates = new ArrayList<String>();
-        expireDates.add("\n, - 20 days \n");
-        expireDates.add("- 3 days \n");
-        expireDates.add("- 50 days \n");
-
-       for (int i = 0; i < productsAvailable.size(); i++){
-            System.out.print(productsAvailable.get(i) );
-            System.out.println(expireDates.get(i));
+       for (int i = 0; i < availableProducts.length; i++){
+            System.out.print(availableProducts[i] + " - " + dateProductArrived[i] + " - " + expireDates[i] + "\n");
         }
 
 
     }
 
-    public static void showTheProductsWeProvide(){
+    public static void showTheProductsWeProvide(){             //Method for the User's 1st choise
         ArrayList <String> productsWeProvide = new ArrayList<String>();
         productsWeProvide.add("\n, Кока Кола - Захар като за диабет - 500ml\n");
         productsWeProvide.add("Пиринско - Младо Пиво - Млада Бира за Млади Хора - 2l\n");
@@ -65,7 +65,15 @@ public class Main {
 
 
     }
-    public static void exitProgram(){
+    public static void showThePlacementOfTheProducts (){
+        ArrayList <String> placeOfTheProducts = new ArrayList<String>();
+        placeOfTheProducts.add("1st raft\n");
+        placeOfTheProducts.add("1st raft\n");
+        placeOfTheProducts.add("1st raft\n");
+        placeOfTheProducts.add("2nd raft\n");
+        placeOfTheProducts.add("2nd raft\n");
+    }
+    public static void exitProgram(){           // Last choise - the method to exit the program ;)
         System.exit(0);
     }
 
